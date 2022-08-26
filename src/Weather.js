598f573c82d2b2518afd1c302e25eeb1eef50966
Olga Zhyzhka <helgajijika@gmail.com>
2022-08-26 17:48:35 +0300

    add WEather component

---
+++ src/Weather.js
@@ -0,0 +1,23 @@
+import React, {useState} from "react";
+import axios from "axios";
+
+export default function Weather(props) {
+    const apiKey = "455cf20328f600486402fed093d6b1c7";
+    const apiUrl = "https://api.openweathermap.org/data/2.5/weather";
+    const [temp, setTemp] = useState(null);
+
+    function showWeatherData(response) {
+        setTemp(response.data.main.temp)
+    }
+    axios
+		.get(`${apiUrl}?q=${props.city}&appid=${apiKey}&units=metric`)
+		.then(showWeatherData);
+    
+        
+    return (
+        <div className="container">
+            <h1>{Math.round(temp)}</h1>
+        </div>
+    )
+}
+
